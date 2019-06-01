a6e9e980bd1fd2653933bb5a92ec1f30aec396b5
murligoel <murli.goel1999@gmail.com>
2019-06-02 01:44:06 +0530

    seperated restController and controller one for api building and other for web and attached html file

--- src/main/java/com/example/SPRINGCRUD/controller/BookController.java
+++ src/main/java/com/example/SPRINGCRUD/controller/BookController.java
@@ -1,42 +1,28 @@
 package com.example.SPRINGCRUD.controller;
 
-import com.example.SPRINGCRUD.model.BookModel;
 import com.example.SPRINGCRUD.services.BookServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-import javax.validation.Valid;
-import java.util.List;
+@Controller
+@RequestMapping("/books")
+public class BookController {
 
-@RestController
-public class BookController{
-
-    @Autowired
     private BookServices bookServices;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public List<BookModel> getAllBooks(){
-        return bookServices.getAllBooks();
+    @Autowired
+    public BookController(BookServices bookServices) {
+        this.bookServices = bookServices;
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public BookModel getBook(@PathVariable long id){
-        return bookServices.getBook(id);
-    }
-
-    @RequestMapping(value = "/name/{bookTitle}", method = RequestMethod.GET)
-    public BookModel getBookByBookTitle(@PathVariable String bookTitle){
-        return bookServices.getBookByBookTitle(bookTitle);
-    }
-
-    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
-    public void deleteBook(@PathVariable long id){
-        bookServices.deleteBook(id);
-    }
-
-    @RequestMapping(value = "/", method = RequestMethod.POST)
-    public BookModel createBook(@Valid @RequestBody BookModel bookModel){
-        System.out.println(bookModel.toString());
-        return bookServices.createBook(bookModel);
+    @RequestMapping(value = {"","/"},method = RequestMethod.GET)
+    public ModelAndView getAllBook(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("books");
+        modelAndView.addObject("books",bookServices.getAllBooks());
+        return modelAndView;
     }
 }
---
+++ src/main/java/com/example/SPRINGCRUD/controller/BookRestController.java
@@ -0,0 +1,43 @@
+package com.example.SPRINGCRUD.controller;
+
+import com.example.SPRINGCRUD.model.BookModel;
+import com.example.SPRINGCRUD.services.BookServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/books")
+public class BookRestController {
+
+    @Autowired
+    private BookServices bookServices;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public List<BookModel> getAllBooks(){
+        return bookServices.getAllBooks();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public BookModel getBook(@PathVariable long id){
+        return bookServices.getBook(id);
+    }
+
+    @RequestMapping(value = "/name/{bookTitle}", method = RequestMethod.GET)
+    public BookModel getBookByBookTitle(@PathVariable String bookTitle){
+        return bookServices.getBookByBookTitle(bookTitle);
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public void deleteBook(@PathVariable long id){
+        bookServices.deleteBook(id);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public BookModel createBook(@Valid @RequestBody BookModel bookModel){
+        System.out.println(bookModel.toString());
+        return bookServices.createBook(bookModel);
+    }
+}
