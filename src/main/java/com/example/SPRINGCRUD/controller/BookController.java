72a16b7bb35144e11e948893971d42e14908494a
murligoel <murli.goel1999@gmail.com>
2019-06-02 02:12:22 +0530

    created form to take input from user

--- src/main/java/com/example/SPRINGCRUD/controller/BookController.java
+++ src/main/java/com/example/SPRINGCRUD/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.example.SPRINGCRUD.controller;
 
+import com.example.SPRINGCRUD.model.BookModel;
 import com.example.SPRINGCRUD.services.BookServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -23,6 +24,15 @@ public class BookController {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("books");
         modelAndView.addObject("books",bookServices.getAllBooks());
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public ModelAndView createBookGet(String msg) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("book_form");
+        modelAndView.addObject("book", new BookModel());
+        modelAndView.addObject("msg", msg);
         return modelAndView;
     }
 }
