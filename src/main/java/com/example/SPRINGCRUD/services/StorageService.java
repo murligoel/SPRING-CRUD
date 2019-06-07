cea8fe59b39c969bbdf30efcf995ea3c0f1be0d7
murligoel <murli.goel1999@gmail.com>
2019-06-08 01:10:02 +0530

    Added upload feature using local host generating url and one can download it

---
+++ src/main/java/com/example/SPRINGCRUD/services/StorageService.java
@@ -0,0 +1,22 @@
+package com.example.SPRINGCRUD.services;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public interface StorageService {
+
+    void init();
+
+    void store(MultipartFile file);
+
+    Stream<Path> loadAll();
+
+    Path load(String filename);
+
+    void deleteAll();
+
+    Resource loadAsResource(String filename);
+}
