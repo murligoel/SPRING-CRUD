a8c330adfcb592feb9fab2bac9e725ce24508ca1
murligoel <murli.goel1999@gmail.com>
2019-06-08 23:47:26 +0530

    Created a DBFile entity to model the file attributes that will be stored in the database

---
+++ src/main/java/com/example/SPRINGCRUD/DBFile.java
@@ -0,0 +1,31 @@
+package com.example.SPRINGCRUD;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "files")
+public class DBFile {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    private String fileName;
+
+    private String fileType;
+
+    @Lob
+    private byte[] data;
+
+    public DBFile() {
+
+    }
+
+    public DBFile(String fileName, String fileType, byte[] data) {
+        this.fileName = fileName;
+        this.fileType = fileType;
+        this.data = data;
+    }
+}
